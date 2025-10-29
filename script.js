30a0c83f9f3739467ef07d082a947550e2331228
ayoub-elkhadir-d <assiacodi@gmail.com>
2025-10-29 16:57:58 +0100

    add button add mitions

--- script.js
+++ script.js
@@ -4,15 +4,13 @@ const menuIcones = document.querySelectorAll('.menu_icone');  // استخدم qu
 
 // حلقة على كل أيقونة لإضافة listener
 menuIcones.forEach(function(icone) {
-    icone.addEventListener('click', function(event) {
-        // منع انتشار الحدث إذا لزم الأمر
-        
+    icone.addEventListener('click', function(event) {        
         
         // البحث عن ul.menu_choix داخل نفس الـ choix_et_menu_div (الأب المباشر)
         const menuChoix = icone.parentElement.querySelector('.menu_choix');
         
         if (menuChoix) {
-            // التبديل بين الإظهار والإخفاء
+
             if (menuChoix.style.display === 'flex') {
                 menuChoix.style.display = 'none';  // إخفاء
             } else {
