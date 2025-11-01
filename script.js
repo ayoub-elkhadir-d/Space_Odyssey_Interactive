0b8aebb285c6e89f6cc1252eb59196a1bb7fe9cc
ayoub-elkhadir-d <assiacodi@gmail.com>
2025-11-01 22:39:53 +0100

    modification in code (filtre et cards validation ..)

--- script.js
+++ script.js
@@ -55,13 +55,14 @@ let container = document.getElementById("misions_div");
 let label = document.getElementById("labell");
 let container_all = document.getElementById("container_");
 let button_cancel_ = document.getElementById("button_cancel");
+
 aficher();
 /***************************************************************************/
-button_cancel_.addEventListener("click",function(){
-container.innerHTML = "";
-aficher()
+button_cancel_.addEventListener("click", function () {
 
-})
+  container.innerHTML = "";
+  aficher();
+});
 /*********************************aficher******************************************/
 function aficher_sur_container_top(index, creat_card, conten) {
   /*--------------pour aficher -----------*/
@@ -95,26 +96,32 @@ function aficher_sur_container_top(index, creat_card, conten) {
 }
 
 function aficher(index) {
-  button_cancel_.style.visibility="hidden"
-  label.innerHTML="Missions"
+  button_cancel_.style.visibility = "hidden";
+  label.innerHTML = "Missions";
   container.innerHTML = "";
   for (var t = 0; t < misions.length; t++) {
     if (misions[t].status == 1) {
-      if (t == index) {
-        container.innerHTML += `
-                      <div class="mission_card mission_card_edit ">  
-                        <div class="choix_et_menu_div">  
-                          <button class="btn_suprimer" onclick="suprimer(${t})"></button>   
-                          <button class="btn_favorit" id="btn_${t}" onclick="favorit(${t})"></button>  
-                        </div>  
-                        <img class="misions_img" src="${misions[t].image}" alt="">  
-                        <input type="text" id="input_name" value="${misions[index].name}">
-                        <input type="text" id="input_agence" value="${misions[index].agency}">
-                        <input type="text" id="input_discription" value="${misions[index].objective}">
-                        <input type="text" id="input_date" value="${misions[index].launchDate}">
-                        <button id="submit" onclick="save(${index})" style="align-self: center;">save</button>
-                      </div>`;
-      }
+     if (t == index) {
+  container.innerHTML += `
+    <div class="mission_card mission_card_edit " id ="card">  
+        <div class="container_label_button">
+          <h1 id="labell" class="label_">Add Misions</h1>
+          <button id="button_cancel_edit" class="button_"></button>
+        </div>
+      <div class="choix_et_menu_div">  
+        <button class="btn_suprimer" onclick="suprimer(${t})"></button>   
+        <button class="btn_favorit" id="btn_${t}" onclick="favorit(${t})"></button>  
+      </div>  
+      <img class="misions_img" src="${misions[t].image}" alt="">  
+      <input type="text" id="input_name" value="${misions[index].name}">
+      <input type="text" id="input_agence" value="${misions[index].agency}">
+      <input type="text" id="input_discription" value="${misions[index].objective}">
+      <input type="text" id="input_date" value="${misions[index].launchDate}">
+      <button id="submit" onclick="save(${index})" style="align-self: center;">save</button>
+    </div>`;
+
+ 
+}
 
       if (t != index) {
         if (misions[t].in_favorite == 0) {
@@ -152,42 +159,30 @@ function aficher(index) {
     }
   }
 }
+
 /**************************************edit*************************************/
 function edit(index) {
   aficher(index);
   container2.innerHTML = "";
-}
-/************************************add***************************************/
 
-
-function submit_mission(){
-
-var legnth_m = misions.length+1
-console.log(misions.length)
-
-
-   var new_mission= {
-    id: legnth_m,
-    status: 1,
-    in_favorite: 0,
-    name: document.getElementById("input_name").value,
-    agency: document.getElementById("input_agence").value,
-    objective: document.getElementById("input_discription").value,
-    launchDate: document.getElementById("input_date").value,
-    image: "images/rosetta.png",
-  };
+  // 
+  const cancelEditBtn = document.getElementById("button_cancel_edit");
  
-   container2.style.display = "none";
-   document.getElementById("misions_div").innerHTML = "";
-     misions.push(new_mission)
-  aficher();
+    cancelEditBtn.addEventListener("click", function () {
+      console.log("cancel edit clicked");
+      aficher(); // 
+    });
   
 }
 
-
-var content_add_card =
-                   `<div class="mission_card mission_card_edit add">   
-                        <h1>add Misions</h1>
+/************************************add***************************************/
+var content_add_card = `
+<div class="mission_card mission_card_edit add" id="add_card">   
+                        
+              <div class="container_label_button">
+                  <h1 id="labell" class="label_">Add Misions</h1>
+                  <button id="button_cancel_add" class="button_"></button>
+                </div>
                         <input type="text" id="input_name"  placeholder="Enter name">
                         <input type="text" id="input_agence" placeholder="Enter agence">
                         <input type="text" id="input_discription" placeholder="Enter description">
@@ -195,19 +190,59 @@ var content_add_card =
                         <button id="submit" onclick="submit_mission()" style="align-self: center;">save</button>
                       </div>`;
 
+function submit_mission() {
+  var card__= document.getElementById("add_card")
+  var input_name_ = document.getElementById("input_name");
+  var input_agence__ = document.getElementById("input_agence");
+  var input_discription_ = document.getElementById("input_discription");
+  var input_date_ = document.getElementById("input_date");
+  var legnth_m = misions.length + 1;
+  if (
+    input_name_.value != "" &&
+    input_agence__.value != "" &&
+    input_discription_.value != "" &&
+    input_date_.value != "") {
+      var new_mission = {
+          id: legnth_m,
+          status: 1,
+          in_favorite: 0,
+          name: input_name_.value,
+          agency: input_agence__.value,
+          objective: input_discription_.value,
+          launchDate: input_date_.value,
+          image: "images/rosetta.png",
+        };
+
+  container2.style.display = "none";
+  document.getElementById("misions_div").innerHTML = "";
+  misions.push(new_mission);
+  aficher();
+  } else {
+   card__.style.boxShadow="1px 1px  10px 5px rgb(201, 0, 0)"
+  }
+
+  
+}
+
 document.getElementById("button_add").addEventListener("click", function () {
   container2.innerHTML = "";
- 
+
   if (container2.style.display == "flex") {
     container2.style.display = "none";
   } else {
     container2.style.display = "flex";
     aficher_sur_container_top(0, true, content_add_card);
-     document.getElementById("input_name").focus()
-   
- 
-  }
+    document.getElementById("input_name").focus();
+
+    document.getElementById("button_cancel_add").addEventListener("click", function () {
+  document.getElementById("add_card").style.display = "none";
 });
+
+  }
+
+});
+
+
 /************************************suprimer***************************************/
 function suprimer(index) {
   misions[index].status = 0;
@@ -217,7 +252,6 @@ function suprimer(index) {
 }
 /***********************************favorit****************************************/
 function favorit(d) {
-
   if (misions[d].in_favorite == 0) {
     misions[d].in_favorite = 1;
     document.getElementById("misions_div").innerHTML = "";
@@ -231,26 +265,42 @@ function favorit(d) {
 }
 /***********************************save****************************************/
 function save(index) {
-  document.getElementById("popupp").style.display = "none";
-  misions[index].name = document.getElementById("input_name").value;
-  misions[index].agency = document.getElementById("input_agence").value;
-  misions[index].objective = document.getElementById("input_discription").value;
-  misions[index].launchDate = document.getElementById("input_date").value;
-  document.getElementById("misions_div").innerHTML = "";
-  aficher();
+  var card_ = document.getElementById("card")
+  var input_name_ = document.getElementById("input_name");
+  var input_agence__ = document.getElementById("input_agence");
+  var input_discription_ = document.getElementById("input_discription");
+  var input_date_ = document.getElementById("input_date");
+  if (
+    input_name_.value != "" &&
+    input_agence__.value != "" &&
+    input_discription_.value != "" &&
+    input_date_.value != "") {
+
+    misions[index].name = input_name_.value;
+    misions[index].agency = input_agence__.value;
+    misions[index].objective = input_discription_.value;
+    misions[index].launchDate = input_date_.value;
+    
+    document.getElementById("misions_div").innerHTML = "";
+    aficher();
+    document.getElementById("popupp").style.display = "none";
+  } else {
+   card_.style.boxShadow="1px 1px  10px 5px rgb(201, 0, 0)"
+  }
+  
 }
+
 
 /********************************afficher_favorits*******************************************/
 function afficher_favorits() {
-   button_cancel_.style.visibility="visible"
+  button_cancel_.style.visibility = "visible";
   container.innerHTML = "";
-  label.innerHTML = "Favorits"
-    for (var b = 0; b < misions.length; b++) {
-      if (misions[b].in_favorite == 1 && misions[b].status != 0) {
-         aficher_sur_container_top(b, false);
-      }
+  label.innerHTML = "Favorits";
+  for (var b = 0; b < misions.length; b++) {
+    if (misions[b].in_favorite == 1 && misions[b].status != 0) {
+      aficher_sur_container_top(b, false);
     }
-  
+  }
 }
 var a = document
   .getElementById("fav_logo")
@@ -261,11 +311,13 @@ var search = document.getElementById("search");
 var text_typing = "";
 search.addEventListener("keyup", (event) => {
   container.innerHTML = "";
-   button_cancel_.style.visibility="visible"
-  label.innerHTML = "Result Search"
+  button_cancel_.style.visibility = "visible";
+  label.innerHTML = "Result Search";
+
   for (var z = 0; z < misions.length; z++) {
     var name_to_lower = misions[z].name.toLowerCase();
-    if (name_to_lower.startsWith(search.value) && misions[z].status != 0) {
+
+    if (name_to_lower.includes(search.value) && misions[z].status != 0) {
       aficher_sur_container_top(z, false);
     }
   }
@@ -290,34 +342,41 @@ function filter_fun() {
       select2.innerHTML += `<option value="${misions[aa].agency}">${misions[aa].agency}</option>`;
       select3.innerHTML += `<option value="${misions[aa].launchDate}">${misions[aa].launchDate}</option>`;
     }
+
     select1.addEventListener("change", function () {
-      container2.innerHTML = "";
+      container.innerHTML = "";
       container4.style.display = "none";
       for (var ae = 0; ae < misions.length; ae++) {
         if (misions[ae].name == select1.value && misions[ae].status != 0) {
+          button_cancel_.style.visibility = "visible";
+          label.innerHTML = "Filtre";
           aficher_sur_container_top(ae, false);
         }
       }
     });
 
     select2.addEventListener("change", function () {
-      container2.innerHTML = "";
+      container.innerHTML = "";
       container4.style.display = "none";
       for (var ar = 0; ar < misions.length; ar++) {
         if (misions[ar].agency == select2.value && misions[ar].status != 0) {
+          button_cancel_.style.visibility = "visible";
+          label.innerHTML = "Filtre";
           aficher_sur_container_top(ar, false);
         }
       }
     });
 
     select3.addEventListener("change", function () {
-      container2.innerHTML = "";
+      container.innerHTML = "";
       container4.style.display = "none";
       for (var ay = 0; ay < misions.length; ay++) {
         if (
           misions[ay].launchDate == select3.value &&
           misions[ay].status != 0
         ) {
+          button_cancel_.style.visibility = "visible";
+          label.innerHTML = "Filtre";
           aficher_sur_container_top(ay, false);
         }
       }
